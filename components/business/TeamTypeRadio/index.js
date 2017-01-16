827960a95795fa35aed98bcd4a1c2a72559fc4a6
wangying02 <wangying02@iwaimai.baidu.com>
2017-01-16 15:55:44 +0800

    merge

--- components/business/TeamTypeRadio/index.js
+++ components/business/TeamTypeRadio/index.js
@@ -1,31 +1,58 @@
+/**
+ * @file 物流方类别单选框组件，暂仅用于取值，不用于设值
+ *       modified by wangying02 <wangying02@iwaimai.baidu.com>
+ *
+ * @author lichun <lichun@iwaimai.baidu.com>
+ * @version 0.0.1
+ *
+ */
 import React, { PropTypes } from 'react';
 import { findDOMNode } from 'react-dom'
 import { Radio } from 'antd';
 
 /**
  * 组件属性申明
- * @name 参数名
+ * @property {object} defaultValue 默认值
+ * @property {function} onChange
  */
 const propTypes = {
+  defaultValue: PropTypes.object,
+  onChange: PropTypes.func,
 }
 
 const RadioButton = Radio.Button;
 const RadioGroup = Radio.Group;
+
+/**
+ * 主 *
+ * @export组件
+
+ * @class TeamTypeRadio
+ * @extends {React.Component}
+ */
 
 const TEAM_TYPES = [
     { label: '自定义', value: 0 },
     { label: '直营', value: 1 },
     { label: '渠道', value: 2 }
 ]
-/**
- * 物流方类别单选框
- * 暂仅用于取值，不用于设值
- */
 export default class TeamTypeRadio extends React.Component {
+  /**
+   * Creates an instance of TeamTypeRadio.
+   *
+   * @memberOf TeamTypeRadio
+   */
+    constructor(props) {
+        super(props);
+    }
+    componentDidMount() {
+    }
+    componentWillReceiveProps(nextProps) {
+    }
     render() {
         const { onChange } = this.props;
         return (
-            <RadioGroup 
+            <RadioGroup
                 defaultValue={0}
                 onChange = { onChange }
             >
